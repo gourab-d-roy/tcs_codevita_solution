92bfd933931215ec5f9e378a6f4de8fbde7c2eb0
gourab dutta roy <67657477+gourab-d-roy@users.noreply.github.com>
2020-08-25 20:37:04 +0530

    the source code for string merge

--- d.py
+++ d.py
@@ -1,33 +1,33 @@
+#code by gourab-d-roy for string merge question tcs codevita season 9 zone 2
+#author:GDR/gourab-d-roy
 ls=[]
 count=dict()
 ct=dict()
 mer=''
-main=input('original.........')
+main=input('original string input.........')
 for letter in main:
     count[letter]=count.get(letter,0)+1
 no=int(input('nos.......'))
 for  i in range(no):
-    mer=mer+input('strs........')
+    mer=mer+input('input strs to be merged........')
 
-#print(mer)
+
 for l in mer:
     ct[l]=ct.get(l,0)+1
 
-print(count)#for my use
-print(ct)#for my use
+
 
 for j in count:
     print (count[j])
     print(ct[j])
 
-    #'''try:#for my use
+   
     if (count[j]==ct[j]):
         ls.append(1)
     else:
         ls.append(False)
-    #except:#for my use
-    #    continue#for my use
-print(ls)
+    
+
 if all(ls)==True:
     print('YES')
 else:
